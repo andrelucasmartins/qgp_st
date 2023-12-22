7c3353b207295044d61032f6ff189c7907ce949c
André Ezequiel <andrelucasmartins@hotmail.com>
2023-12-22 13:48:49 -0300

    chore: add params nextConfig

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: process.env.NODE_ENV === "production" ? "export" : undefined,
+  images: {
+    unoptimized: true,
+  }
+}
 
 module.exports = nextConfig
